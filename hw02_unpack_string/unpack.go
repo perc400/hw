0b43db222cfa2a59b6f79d05810ca1fc2d01a240
a.shenderey <a.shenderey@mti-lab.com>
2025-08-16 14:57:46 +0300

    HW2 is completed

--- hw02_unpack_string/unpack.go
+++ hw02_unpack_string/unpack.go
@@ -2,11 +2,51 @@ package hw02unpackstring
 
 import (
 	"errors"
+	"strconv"
+	"strings"
 )
 
 var ErrInvalidString = errors.New("invalid string")
 
-func Unpack(_ string) (string, error) {
-	// Place your code here.
-	return "", nil
+func Unpack(input string) (string, error) {
+	if input == "" {
+		return "", nil
+	}
+
+	runes := []rune(input)
+	if _, err := strconv.Atoi(string(runes[0])); err == nil {
+		return "", ErrInvalidString
+	}
+
+	for index := 0; index < len(runes)-1; index++ {
+		if _, err := strconv.Atoi(string(runes[index])); err == nil {
+			if _, err := strconv.Atoi(string(runes[index+1])); err == nil {
+				return "", ErrInvalidString
+			}
+			continue
+		}
+	}
+
+	var builder strings.Builder
+	for index := 0; index < len(runes); index++ {
+		if index == len(runes)-1 {
+			if _, err := strconv.Atoi(string(runes[index])); err != nil {
+				builder.WriteRune(runes[index])
+			}
+			break
+		}
+
+		if _, err := strconv.Atoi(string(runes[index])); err == nil {
+			continue
+		}
+
+		times, err := strconv.Atoi(string(runes[index+1]))
+		if err != nil {
+			builder.WriteRune(runes[index])
+		} else {
+			builder.WriteString(strings.Repeat(string(runes[index]), times))
+		}
+	}
+
+	return builder.String(), nil
 }
