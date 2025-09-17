f89cb0d0af15a29ac942f53d5504e2170a43b844
a.shenderey <a.shenderey@mti-lab.com>
2025-09-18 02:59:38 +0300

    Fixing the data race (insecure access to 'errorsCount')

--- hw05_parallel_execution/run.go
+++ hw05_parallel_execution/run.go
@@ -10,20 +10,6 @@ var ErrErrorsLimitExceeded = errors.New("errors limit exceeded")
 
 type Task func() error
 
-func doTasks(waitGroup *sync.WaitGroup, once *sync.Once, errorsCount *int32, maxErrorsCount int, tasksChannel chan Task, doneChannel chan struct{}) {
-	defer waitGroup.Done()
-
-	for task := range tasksChannel {
-		if task() != nil {
-			atomic.AddInt32(errorsCount, 1)
-			if *errorsCount >= int32(maxErrorsCount) {
-				once.Do(func() { close(doneChannel) })
-				return
-			}
-		}
-	}
-}
-
 // Run starts tasks in n goroutines and stops its work when receiving m errors from tasks.
 func Run(tasks []Task, n, m int) error {
 	if m <= 0 {
@@ -32,14 +18,25 @@ func Run(tasks []Task, n, m int) error {
 
 	var wg sync.WaitGroup
 	var once sync.Once
-	var errorsCount int32
+	var errorsCount int64
 
 	tasksCh := make(chan Task)
 	doneCh := make(chan struct{})
 
 	for i := 0; i < n; i++ {
 		wg.Add(1)
-		go doTasks(&wg, &once, &errorsCount, m, tasksCh, doneCh)
+		go func(wg *sync.WaitGroup, once *sync.Once) {
+			defer wg.Done()
+			for task := range tasksCh {
+				if task() != nil {
+					atomic.AddInt64(&errorsCount, 1)
+					if atomic.LoadInt64(&errorsCount) >= int64(m) {
+						once.Do(func() { close(doneCh) })
+						return
+					}
+				}
+			}
+		}(&wg, &once)
 	}
 
 	breakSending := false
