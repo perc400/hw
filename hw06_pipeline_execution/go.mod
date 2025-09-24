30ff0c095a78e162c6ed880e2bfd5b158609ee26
a.shenderey <a.shenderey@mti-lab.com>
2025-09-24 23:45:35 +0300

    HW6 is completed

--- hw06_pipeline_execution/go.mod
+++ hw06_pipeline_execution/go.mod
@@ -1,8 +1,8 @@
-module github.com/fixme_my_friend/hw06_pipeline_execution
+module github.com/perc400/hw/hw06_pipeline_execution
 
-go 1.23
+go 1.23.11
 
-require github.com/stretchr/testify v1.10.0
+require github.com/stretchr/testify v1.11.1
 
 require (
 	github.com/davecgh/go-spew v1.1.1 // indirect
--- hw06_pipeline_execution/pipeline.go
+++ hw06_pipeline_execution/pipeline.go
@@ -8,7 +8,44 @@ type (
 
 type Stage func(in In) (out Out)
 
+func helper(in In, done In) Out {
+	out := make(Bi)
+	go func() {
+		defer close(out)
+		for {
+			select {
+			case <-done:
+				go func() {
+					for data := range in {
+						_ = data
+					}
+				}()
+				return
+			case data, ok := <-in:
+				if !ok {
+					return
+				}
+				select {
+				case out <- data:
+				case <-done:
+					go func() {
+						for data := range in {
+							_ = data
+						}
+					}()
+					return
+				}
+			}
+		}
+	}()
+	return out
+}
+
 func ExecutePipeline(in In, done In, stages ...Stage) Out {
-	// Place your code here.
-	return nil
+	out := in
+
+	for _, stage := range stages {
+		out = stage(helper(out, done))
+	}
+	return out
 }
